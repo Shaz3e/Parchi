4fd626875a5f6d7e280289a10affcd8295117e28
Shahrukh A. Khan <iam@shaz3e.com>
2024-08-21 00:54:20 +0500

    Validation Doc Link Added

--- app/Http/Requests/BaseFormRequest.php
+++ app/Http/Requests/BaseFormRequest.php
@@ -9,6 +9,7 @@ class BaseFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     * @link https://laravel.com/docs/11.x/validation#available-validation-rules
      */
     public function authorize(): bool
     {
